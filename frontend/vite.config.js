5089e7aa14ee32db30f7e395f7a408052398a7ac
Ramprasadns <ramaigen2025@gmail.com>
2025-11-01 22:05:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,18 @@
-export default {}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  root: '.',          // points to current folder
+  base: './',         // relative assets
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})

2af1b31208bcd4b40d81c79e0619b1af2a0ecab9
Ramprasadns <ramaigen2025@gmail.com>
2025-11-01 18:08:04 +0530

    Delete frontend directory

--- frontend/src/config.js
+++
@@ -1,1 +0,0 @@
-export const API_BASE = "https://studiox-cloud-1.onrender.com";
--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base can be left default since we will copy built files to public root
-export default defineConfig({
-  plugins: [react()],
-})

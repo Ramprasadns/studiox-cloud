8671a74dd40cc2316daf923866afcc5cec9fed0c
Ramprasadns <ramaigen2025@gmail.com>
2025-11-01 16:47:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// base can be left default since we will copy built files to public root
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-});
+})

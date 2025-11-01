4a992541944d4a8adfac014ab3be07f53f56917c
Ramprasadns <ramaigen2025@gmail.com>
2025-11-01 19:08:22 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,11 @@
-import React from 'react'\nimport { createRoot } from 'react-dom/client'\nimport App from './App.jsx'\nimport './index.css'\ncreateRoot(document.getElementById('root')).render(<App />)\n
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

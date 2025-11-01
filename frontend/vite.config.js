9295bf10fc6616c04089b6d5fb2f7bd54c12fc10
Ramprasadns <ramaigen2025@gmail.com>
2025-11-01 18:50:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,14 @@
-import { defineConfig } from 'vite'\nimport react from '@vitejs/plugin-react'\nexport default defineConfig({plugins:[react()],build:{outDir:'dist'}})\n
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+})
